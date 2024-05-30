da768c651a0c6b2b3b9cb8d436e732d4545c54df
Divyanshu Gupta <divyanshu.gupta01@indiamart.com>
2024-05-31 01:14:29 +0530

    adding check for already present record and correction in where queries

--- api/controllers/identify.go
+++ api/controllers/identify.go
@@ -35,13 +35,52 @@ func LinkIdentity(context *gin.Context){
 	
 	 var sameEmailContacts,samePhoneContacts []models.Contact
 
-	 matchedPhoneResults := database.DB.Where("phone_number=?",userDetails.PhoneNumber).Find(&samePhoneContacts)
-	 matchedEmailResults := database.DB.Where("email=?",userDetails.Email).Find(&sameEmailContacts)
+	 matchedPhoneResults := database.DB.Where("phone_number=? and link_precedence=?",userDetails.PhoneNumber,"primary").Find(&samePhoneContacts)
+	 matchedEmailResults := database.DB.Where("email=? and link_precedence=?",userDetails.Email,"primary").Find(&sameEmailContacts)
 
 	 if matchedEmailResults.RowsAffected > 0 && matchedPhoneResults.RowsAffected > 0 {
 		 // found matched records with both email and phone
 	 } else if matchedEmailResults.RowsAffected > 0 || matchedPhoneResults.RowsAffected > 0 {
 		 // found matched records based on either the Phone or Email
+		 var parentContactID uint
+		 if matchedPhoneResults.RowsAffected > 0 { 
+
+			//need to check first if the record is new or not
+			if userDetails.Email == sameEmailContacts[0].Email {
+				 //Email and Phone are same so not a new record
+				 context.JSON(http.StatusContinue,gin.H{"status":"record already present"})
+				 return
+			}	
+			
+			parentContactID = samePhoneContacts[0].ID
+		 } else {
+
+			if userDetails.PhoneNumber == samePhoneContacts[0].PhoneNumber {
+				//Email and Phone are same so not a new record
+				context.JSON(http.StatusContinue,gin.H{"status":"record already present"})
+				return
+		   }	
+			 
+			 parentContactID = sameEmailContacts[0].ID
+		 }
+		
+		newContact := models.Contact{
+			PhoneNumber: userDetails.PhoneNumber,
+			Email:userDetails.Email,
+			LinkedID:parentContactID,
+			LinkPrecedence : "secondary",
+			
+		}
+
+		saveResult := database.DB.Save(&newContact)
+
+		if saveResult.RowsAffected > 0 {
+			context.JSON(http.StatusOK,gin.H{"email":newContact.Email,"phoneNumber":newContact.PhoneNumber})
+		} else {
+			context.JSON(http.StatusBadRequest,gin.H{"status":"RECORD NOT SAVED" ,"error":saveResult.Error})
+		}
+		  
+
 	 } else {
 		 //need to create the new contact since  there is no contact with this phone/email
 
